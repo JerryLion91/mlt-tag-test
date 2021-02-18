dab94a9b23d668d8430f0e92d5c872b4711ab22e
JerryPC <jeremaios@gmail.com>
2021-02-18 11:38:09 -0300

    move helpers folder and change app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+// import TagContructorPage from './components/pages/TagConstructorPage';
+// import LoadingPage from './components/pages/LoadingPage';
+// import HomePage from './components/pages/HomePage';
+// import SignInRegisterPage from './components/pages/SignInRegisterPage';
+
+import { ProvideAuth } from './helpers/use-auth.js';
+import { useAuth } from './helpers/use-auth.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Testing changes</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Get auth state and re-render anytime it changes
+  const auth = useAuth();
+
+  // timer 1 seg to simulate loading
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <ProvideAuth>Hello World</ProvideAuth>;
 }
 
 export default App;
