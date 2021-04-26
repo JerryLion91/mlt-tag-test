86efe31f980c021579e89cb089193f1c16b36d54
JerryPC <jeremaios@gmail.com>
2021-04-26 10:35:05 -0300

    add new images

--- src/components/TagDisplay.js
+++ src/components/TagDisplay.js
@@ -4,30 +4,59 @@ import Button from './Button';
 export default function TagDisplay() {
   const TAGS = [
     {
-      description: 'Tag red and white',
-      src: 'Noah Stewart v13 v1 redWhite.png',
+      description: 'Tag blue and yellow',
+      src: 'Noah_BlueYellow.png',
     },
     {
-      description: 'Tag yellow and blue',
-      src: 'Noah Stewart v13 v1 yellowBlue.png',
+      description: 'Tag red and white',
+      src: 'Noah_RedWhite.png',
+    },
+    {
+      description: 'Tag yellow and black',
+      src: 'Noah_YellowBlack.png',
     },
   ];
   const [selectedTagIndex, setSelectedTagIndex] = React.useState(0);
 
-  const toggleIndex = () => {
-    if (selectedTagIndex === 0) setSelectedTagIndex(1);
-    if (selectedTagIndex === 1) setSelectedTagIndex(0);
+  const toggleIndex = (type) => {
+    const maxIndex = TAGS.length - 1;
+    switch (type) {
+      case '-':
+        if (selectedTagIndex === 0) {
+          setSelectedTagIndex(maxIndex);
+        } else {
+          setSelectedTagIndex((prevIndex) => prevIndex - 1);
+        }
+        break;
+      case '+':
+        if (selectedTagIndex === maxIndex) {
+          setSelectedTagIndex(0);
+        } else {
+          setSelectedTagIndex((prevIndex) => prevIndex + 1);
+        }
+        break;
+      default:
+        break;
+    }
   };
 
   return (
     <div style={styles.divFlexRow}>
-      <Button onClick={toggleIndex} icon={'keyboard_arrow_left'} text={''} />
+      <Button
+        onClick={() => toggleIndex('-')}
+        icon={'keyboard_arrow_left'}
+        text={''}
+      />
       <img
         style={{ margin: 'auto', width: '75vw', maxWidth: '350px' }}
         src={TAGS[selectedTagIndex].src}
         alt={TAGS[selectedTagIndex].description}
       />
-      <Button onClick={toggleIndex} icon={'keyboard_arrow_right'} text={''} />
+      <Button
+        onClick={() => toggleIndex('+')}
+        icon={'keyboard_arrow_right'}
+        text={''}
+      />
     </div>
   );
 }
