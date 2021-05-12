3e6b2c9d6726391e4e060fb7c7b340e833397585
JerryPC <jeremaios@gmail.com>
2021-05-12 15:09:30 -0300

    add paypal logo

--- src/components/pages/TagPaymentPage.js
+++ src/components/pages/TagPaymentPage.js
@@ -59,7 +59,7 @@ export default function TagPaymentPage({ TAGs, shippingPrice }) {
                   : 'radio_button_unchecked'
               }
             >
-              Paypal
+              <img src={'paypal-5-226456.png'} alt="Paypal" />
             </Button>
           </div>
         </div>
